4704d887d4140b3f10a335c50569345eeb8c3f4f
Eugene Sevastianov <eugene.sevastianov@facetdata.com>
2019-10-18 12:52:03 +0300

    PLATFORM-126: rate and duration

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 ### Perf Test
 Perf test for Intake service.
 
-The test sends POST HTTP request to Intake with no time/rate limit.
-Batch size is fixed to 100 jsons. 
-There are two env vars to specify URL and API Key: `INTAKE_URL` and `INTAKE_KEY`. 
+The test sends HTTP POST request to Intake.
+Batch size is fixed to 100 jsons.
+ 
+You may control the process using the following env vars: 
++ `INTAKE_URL` - Intake endpoint
++ `INTAKE_KEY` - API key
++ `QUERY_RATE` - the request rate per time unit (e.g. 100, 100/s, 100/m), 0 - no limit
++ `TEST_DURATION` - amount of time to issue request (e.g. 1s, 5s, 100s), 0 - no limit
 
 ### Under the hood
 Based on [Vegeta](https://github.com/tsenart/vegeta).
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-# Substitute Intake URL and API key and pass json config to Vegeta
-jq -ncM --arg intake_url $INTAKE_URL --arg intake_key $INTAKE_KEY \
-'{method: "POST", url: $intake_url, header: {Authorization: ["Basic " + ($intake_key | @base64)]}}' | \
-vegeta attack -format=json -body events.json | vegeta report
+# Substitute key env vars and pass json config to Vegeta
+jq -ncM \
+--arg intake_url $INTAKE_URL \
+--arg intake_key $INTAKE_KEY \
+--arg query_rate $QUERY_RATE \
+--arg test_duration $TEST_DURATION \
+'{
+  method: "POST",
+  url: $intake_url,
+  rate: $query_rate,
+  duration: $test_duration,
+  header: {Authorization: ["Basic " + ($intake_key | @base64)]}
+}' | \
+vegeta attack -format=json -body events.json | \
+vegeta report
